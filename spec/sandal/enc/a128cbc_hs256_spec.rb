388d3c464e6249598e148a53261f9d404049afab
Greg Beech <greg@gregbeech.com>
2013-04-04 22:48:12 +0100

    Added some more tests for AES/CBC encryption.

--- spec/sandal/enc/a128cbc_hs256_spec.rb
+++ spec/sandal/enc/a128cbc_hs256_spec.rb
@@ -1,5 +1,8 @@
 require 'helper'
 require 'openssl'
+require 'securerandom'
+
+# TODO: These tests are really for the Sandal module rather than just the algorithm -- move them!
 
 describe Sandal::Enc::A128CBC_HS256 do
 
@@ -37,8 +40,19 @@ describe Sandal::Enc::A128CBC_HS256 do
     end }.to raise_error Sandal::TokenError, 'Invalid integrity value.'
   end
 
-  it 'can encrypt and decrypt tokens with the RSA1_5 algorithm' do
+  it 'can encrypt and decrypt tokens with the "dir" algorithm' do
     payload = 'Some text to encrypt'
+    content_master_key = SecureRandom.random_bytes(16)
+
+    encrypter = Sandal::Enc::A128CBC_HS256.new(Sandal::Enc::Alg::Direct.new(content_master_key))
+    token = Sandal.encrypt_token(payload, encrypter)
+
+    output = Sandal.decrypt_token(token) { encrypter }
+    output.should == payload
+  end
+
+  it 'can encrypt and decrypt tokens with the RSA1_5 algorithm' do
+    payload = 'Some other text to encrypt'
     rsa = OpenSSL::PKey::RSA.new(2048)
 
     encrypter = Sandal::Enc::A128CBC_HS256.new(Sandal::Enc::Alg::RSA1_5.new(rsa.public_key))
@@ -47,7 +61,19 @@ describe Sandal::Enc::A128CBC_HS256 do
     output = Sandal.decrypt_token(token) do 
       Sandal::Enc::A128CBC_HS256.new(Sandal::Enc::Alg::RSA1_5.new(rsa))
     end
+    output.should == payload
+  end
 
+  it 'can encrypt and decrypt tokens with the RSA-OAEP algorithm' do
+    payload = 'Some more text to encrypt'
+    rsa = OpenSSL::PKey::RSA.new(2048)
+
+    encrypter = Sandal::Enc::A128CBC_HS256.new(Sandal::Enc::Alg::RSA_OAEP.new(rsa.public_key))
+    token = Sandal.encrypt_token(payload, encrypter)
+
+    output = Sandal.decrypt_token(token) do 
+      Sandal::Enc::A128CBC_HS256.new(Sandal::Enc::Alg::RSA_OAEP.new(rsa))
+    end
     output.should == payload
   end
 
