87843c0781d06415b2e73c7d793ccedd8afcc337
Greg Beech <greg@gregbeech.com>
2013-06-07 12:31:47 +0100

    Added a bunch more encryption tests.

--- spec/sandal/enc/shared_examples.rb
+++ spec/sandal/enc/shared_examples.rb
@@ -38,12 +38,70 @@ shared_examples "invalid tokens" do |enc_class|
 
   context "#decrypt" do
 
-    it "raises an InvalidTokenError when the integrity value is missing" do
+    def test_decrypt_mangled_token(enc_class)
       content_encryption_key = SecureRandom.random_bytes(enc_class::KEY_SIZE / 8)
       enc = enc_class.new(Sandal::Enc::Alg::Direct.new(content_encryption_key))
       token = enc.encrypt("", "any old payload")
-      token = token.split(".").take(4).join # drop integrity value
+      token_parts = token.split(".")
+      yield token_parts
+      token = token_parts.join(".")
       expect { enc.decrypt(token) }.to raise_error Sandal::InvalidTokenError
+    end
+
+    def test_decrypt_token_with_missing_part(enc_class, part_index)
+      test_decrypt_mangled_token(enc_class) do |token_parts|
+        token_parts.delete_at(part_index)
+      end
+    end
+
+    it "raises an InvalidTokenError when the header is missing" do
+      test_decrypt_token_with_missing_part(enc_class, 0)
+    end
+
+    it "raises an InvalidTokenError when the encrypted key is missing" do
+      test_decrypt_token_with_missing_part(enc_class, 1)
+    end
+
+    it "raises an InvalidTokenError when the IV is missing" do
+      test_decrypt_token_with_missing_part(enc_class, 2)
+    end
+
+    it "raises an InvalidTokenError when the encrypted data is missing" do
+      test_decrypt_token_with_missing_part(enc_class, 3)
+    end
+
+    it "raises an InvalidTokenError when the integrity value is missing" do
+      test_decrypt_token_with_missing_part(enc_class, 4)
+    end
+
+    it "raises an InvalidTokenError when the header value is invalid" do
+      test_decrypt_mangled_token(enc_class) do |token_parts|
+        token_parts[0] = token_parts[4]
+      end
+    end
+
+    it "raises an InvalidTokenError when the encrypted key is invalid" do
+      test_decrypt_mangled_token(enc_class) do |token_parts|
+        token_parts[1] = token_parts[4]
+      end
+    end
+
+    it "raises an InvalidTokenError when the IV is invalid" do
+      test_decrypt_mangled_token(enc_class) do |token_parts|
+        token_parts[2] = token_parts[4]
+      end
+    end
+
+    it "raises an InvalidTokenError when the encrypted data is invalid" do
+      test_decrypt_mangled_token(enc_class) do |token_parts|
+        token_parts[3] = token_parts[4]
+      end
+    end
+
+    it "raises an InvalidTokenError when the integrity value is invalid" do
+      test_decrypt_mangled_token(enc_class) do |token_parts|
+        token_parts[4] = token_parts[0]
+      end
     end
 
   end
